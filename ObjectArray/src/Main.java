00b329727228e8521dbe102cd68e78556334fd03
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-05-22 23:43:26 +0000

    Add files via upload

---
+++ ObjectArray/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mustang", "Red");
+        Car car2 = new Car("Corvette", "Blue");
+        Car car3 = new Car("Charger", "Yellow");
+
+        Car[] cars = {car1, car2, car3};
+
+        for (Car car : cars) {
+            car.drive();
+        }
+    }
+}
